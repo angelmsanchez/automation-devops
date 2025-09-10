de41759b17d5638f464470f3b70d6bb74773afcc
Angel Munoz <angel.munoz.sanchez@avanade.com>
2025-09-10 11:31:21 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def saludar(nombre):
+  """Esta función saluda a la persona pasada como parámetro."""
+  mensaje = f"¡Hola, {nombre}!"
+  return mensaje
+
+# Llamar a la función
+print(saludar("Mundo"))
+# Salida: ¡Hola, Mundo!
